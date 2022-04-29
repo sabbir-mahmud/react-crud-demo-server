19f1e4c82648b0066d94e41cdc111531892025c0
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2022-04-29 18:17:32 +0600

    test server adde

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+/**
+ * --------------------------------------------------
+ * root route
+ * --------------------------------------------------
+ */
+
+app.get('/', (req, res) => {
+    res.send('isp warehouse server running...');
+})
+
+
+/**
+ * --------------------------------------------------
+ * server listening
+ * --------------------------------------------------
+ */
 app.listen(port, () => {
     console.log(`Server started on port... ${port}`);
 })
